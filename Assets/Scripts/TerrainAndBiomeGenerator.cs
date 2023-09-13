c081cb69511435558673f89099dadcea03f9eb00
Jonster123 <80619924+Jonster123@users.noreply.github.com>
2023-09-13 17:25:52 +1000

    Added Beach Generation
    Added Beach Generation around Ocean Spawns

--- Assets/Scripts/TerrainAndBiomeGenerator.cs
+++ Assets/Scripts/TerrainAndBiomeGenerator.cs
@@ -29,14 +29,20 @@ public class TerrainAndBiomeGenerator : MonoBehaviour
     [Header("Biome Settings")]
     public List<Biome> biomes;
 
+    // To keep track of instantiated blocks
+    private GameObject[,] blockMap;
+
     private void Start()
     {
-        Random.InitState(seed); // Initialize random number generator with the seed
+        Random.InitState(seed);
+        blockMap = new GameObject[width, depth]; // Initialize blockMap
         GenerateTerrainAndBiomes();
     }
 
     void GenerateTerrainAndBiomes()
     {
+        Biome[,] biomeMap = new Biome[width, depth];
+
         for (int z = 0; z < depth; z++)
         {
             for (int x = 0; x < width; x++)
@@ -45,11 +51,24 @@ public class TerrainAndBiomeGenerator : MonoBehaviour
                 float moisture = Mathf.PerlinNoise(seed + (x + width) / moistureScale, seed + (z + depth) / moistureScale);
 
                 Biome biome = DetermineBiome(altitude, moisture);
+                biomeMap[x, z] = biome;
 
                 if (biome != null)
                 {
-                    Instantiate(biome.blockPrefab, new Vector3(x, altitude * scale, z), Quaternion.identity);
+                    GameObject newBlock = Instantiate(biome.blockPrefab, new Vector3(x, altitude * scale, z), Quaternion.identity);
+                    blockMap[x, z] = newBlock; // Store the new block
                     PlaceMesh(biome, new Vector3(x, altitude * scale + 1, z));
+                }
+            }
+        }
+
+        for (int z = 1; z < depth - 1; z++)
+        {
+            for (int x = 1; x < width - 1; x++)
+            {
+                if (biomeMap[x, z]?.name == "Ocean")
+                {
+                    ReplaceAdjacentWithBeach(x, z, biomeMap);
                 }
             }
         }
@@ -75,4 +94,42 @@ public class TerrainAndBiomeGenerator : MonoBehaviour
             Instantiate(biome.meshPrefabs[Random.Range(0, biome.meshPrefabs.Count)], position, Quaternion.identity);
         }
     }
+
+    void ReplaceAdjacentWithBeach(int x, int z, Biome[,] biomeMap)
+    {
+        Biome beachBiome = biomes.Find(b => b.name == "Beach");
+        if (beachBiome == null) return;
+
+        int range = 10; // Change this value to adjust the range. E.g., 2 for a 5x5 grid, 1 for a 3x3 grid.
+
+        for (int dx = -range; dx <= range; dx++)
+        {
+            for (int dz = -range; dz <= range; dz++)
+            {
+                int newX = x + dx;
+                int newZ = z + dz;
+
+                // Check bounds
+                if (newX >= 0 && newX < width && newZ >= 0 && newZ < depth)
+                {
+                    if (biomeMap[newX, newZ]?.name != "Ocean")
+                    {
+                        GameObject oldBlock = blockMap[newX, newZ];
+                        if (oldBlock != null)
+                        {
+                            Destroy(oldBlock); // Remove the existing block
+                        }
+
+                        float altitude = Mathf.PerlinNoise(seed + newX / scale, seed + newZ / scale);
+                        GameObject newBlock = Instantiate(beachBiome.blockPrefab, new Vector3(newX, altitude * scale, newZ), Quaternion.identity);
+                        blockMap[newX, newZ] = newBlock; // Store the new block
+                        biomeMap[newX, newZ] = beachBiome; // Update the biomeMap
+
+                        PlaceMesh(beachBiome, new Vector3(newX, altitude * scale + 1, newZ));
+                    }
+                }
+            }
+        }
+    }
+
 }
